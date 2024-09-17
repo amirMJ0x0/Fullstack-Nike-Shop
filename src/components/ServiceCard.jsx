bc5bbcfedb8bdbb7f41c8f440d0eac3899f89e3a
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2024-09-18 01:24:23 +0330

    feat (landing page): add ServiceCard component and develop Services section

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,15 @@
+const ServiceCard = ({ imgURL, label, subtext }) => {
+  return (
+    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rouded-[20px] px-10 py-16 shodow-3xl">
+      <div className="flex size-11 justify-center items-center bg-coral-red rounded-full">
+        <img src={imgURL} alt={label} width={24} height={24} />
+      </div>
+      <h3 className="font-palanquin font-bold leading-normal text-3xl mt-5">
+        {label}
+      </h3>
+      <p className="info-text mt-3 break-words">{subtext}</p>
+    </div>
+  );
+};
+
+export default ServiceCard;
