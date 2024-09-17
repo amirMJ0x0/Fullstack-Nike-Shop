1102fea0dc6d59834446e064fe062252c9204e29
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2024-09-18 01:00:37 +0330

    feat (landing page): develop superQuality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,9 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className="flex items-center justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
       {label}
-      <img src={iconURL} alt="" className="ml-2 rounded-full size-5" />
+      {iconURL && (
+        <img src={iconURL} alt="" className="ml-2 rounded-full size-5" />
+      )}
     </button>
   );
 };
