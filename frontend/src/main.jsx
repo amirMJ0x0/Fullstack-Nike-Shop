0321c7449a7641cd5d9f10ab2f4fdcf904180bb7
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2024-10-19 22:42:04 +0330

    Add and Initializing React Query and Chakra UI to project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/index.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const queryClient = new QueryClient({});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
