cde2aaacf5148b56475bc11edb499866dd1d4ecd
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2024-10-27 10:28:15 +0330

    Update ProductCard UI and Implement logic of PopularProducts based on sellCount

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,28 +1,78 @@
-import { Card, CardBody, Image, Stack, Heading, Text } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Skeleton,
+  SkeletonText,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { FaStar } from "react-icons/fa";
 
-const ProductCard = ({ imageUrl, name, price, description }) => {
-  console.log(imageUrl, name, price);
-
+const ProductCard = ({ _id, imageUrl, name, price, score, isLoading }) => {
   return (
-    <Card maxW="sm" data-aos="fade-up" shadow="base">
+    <Card maxW="sm" data-aos="fade-up" shadow="none">
       <CardBody>
-        <Image
-          src={imageUrl}
-          alt="Green double couch with wooden legs"
-          borderRadius="lg"
-          position="relative"
-        />
-        <Stack mt="6" spacing="3">
-          <Text className="flex items-center gap-2">
-            <FaStar className="text-yellow-400 text-2xl" /> (4.5)
-          </Text>
-          <Heading size="md">{name}</Heading>
+        {isLoading ? (
+          <>
+            <Skeleton borderRadius="2xl" className="size-72"></Skeleton>
 
-          <Text color="coral" fontSize="2xl" fontWeight="semibold">
-            {price}$
-          </Text>
-        </Stack>
+            <SkeletonText
+              mt="4"
+              noOfLines={1}
+              spacing="4"
+              skeletonHeight="4"
+              width={"5rem"}
+            />
+
+            <SkeletonText
+              mt="1"
+              noOfLines={1}
+              spacing="4"
+              skeletonHeight="4"
+              width={"80%"}
+            />
+            <SkeletonText
+              mt="5"
+              noOfLines={1}
+              spacing="4"
+              skeletonHeight="3"
+              width={"4rem"}
+            />
+          </>
+        ) : (
+          <>
+            <div
+              className={`size-72 bg-cover ${
+                !isLoading && "bg-card"
+              } flex justify-center items-center`}
+            >
+              <Image
+                src={imageUrl[0]}
+                alt={name}
+                borderRadius="lg"
+                position="relative"
+                w={"90%"}
+              />
+            </div>
+
+            <Stack mt="6" spacing="3">
+              <Text className="flex items-center gap-2">
+                <FaStar className="text-yellow-400 text-2xl" /> ({score})
+              </Text>
+
+              <Link to={`/products/${_id}`}>
+                <Heading size="md">{name}</Heading>
+              </Link>
+
+              <Text color="coral" fontSize="2xl" fontWeight="semibold">
+                {price}$
+              </Text>
+            </Stack>
+          </>
+        )}
       </CardBody>
     </Card>
   );
--- frontend/src/sections/PopularProducts.jsx
+++ frontend/src/sections/PopularProducts.jsx
@@ -1,7 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
 import ProductCard from "../components/ProductCard";
-import { products } from "../constants";
+import { getAllProducts } from "../../services/productServices";
+import { CgSpinner } from "react-icons/cg";
+import useSortedProducts from "../hooks/useSortedProducts";
 
 const PopularProducts = () => {
+  const { data, error, isLoading, isSuccess } = useQuery({
+    queryKey: ["products"],
+    queryFn: getAllProducts,
+  });
+  const popularProducts = useSortedProducts(data, "popularity") || [];
+
+  if (isLoading) {
+    return (
+      <div className="fixed top-1/3 left-1/2 animate-spin text-4xl text-coral-red max-container">
+        <CgSpinner />
+      </div>
+    );
+  }
+  if (error) {
+    return <div className="max-container">{error}</div>;
+  }
+  // فیلتر کردن و مرتب‌سازی محصولات بر اساس sellCount
+  // const popularProducts = data
+  //   .filter((product) => product.sellCount > 38) // فقط محصولاتی که فروش داشته‌اند
+  //   .sort((a, b) => b.sellCount - a.sellCount) // مرتب‌سازی به صورت نزولی
+  //   .slice(0, 4); // انتخاب 10 محصول پرفروش
+
   return (
     <section id="products" className="max-container max-sm:mt-12">
       <div className="flex flex-col justify-start gap-5">
@@ -17,8 +42,8 @@ const PopularProducts = () => {
         </p>
       </div>
       <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14">
-        {products.map((product) => (
-          <ProductCard key={product.name} {...product._id} />
+        {popularProducts?.slice(0, 4).map((product) => (
+          <ProductCard key={product._id} {...product} />
         ))}
       </div>
     </section>
