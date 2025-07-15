faa335c891a49d6c3b1181ae04942bcf819cfc70
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2025-07-15 22:39:19 +0330

    fix: The configuration of cookies is now proper for production

--- backend/utils/finalizeAuth.js
+++ backend/utils/finalizeAuth.js
@@ -1,6 +1,9 @@
 const { generateTokens } = require("./generateTokens");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const finalizeAuth = async (res, user) => {
+    const isProduction = process.env.NODE_ENV === "production";
     const { accessToken, refreshToken } = generateTokens(user._id);
 
     // Update user with refresh token
@@ -10,15 +13,15 @@ const finalizeAuth = async (res, user) => {
 
     res.cookie('accessToken', accessToken, {
         httpOnly: true,
-        secure: true,
-        sameSite: "none", // for allowing cross-site
+        secure: isProduction,
+        sameSite: isProduction ? "none" : "lax", // for allowing cross-site
         maxAge: 15 * 60 * 1000,
     });
 
     res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
-        secure: true,
-        sameSite: "none",
+        secure: isProduction,
+        sameSite: isProduction ? "none" : "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
