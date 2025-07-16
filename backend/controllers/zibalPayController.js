c5f48afb43d53311c007d75d293f75553f3c3b1d
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2025-07-17 00:29:43 +0330

    fix: payment error fixed , now user can pay order

--- backend/controllers/zibalPayController.js
+++ backend/controllers/zibalPayController.js
@@ -5,21 +5,23 @@ dotenv.config();
 
 const dollarPrice = 80000
 const createZibalRequest = async (req, res) => {
-    const { amount, orderId, callbackUrl } = req.body;
+    const { amount, orderId: orderNumber, callbackUrl } = req.body;
     try {
         const zibalRes = await axios.post("https://gateway.zibal.ir/v1/request", {
             merchant: process.env.ZIBAL_SANDBOX_KEY,
             amount: amount * dollarPrice,
             callbackUrl,
-            orderId,
-            description: `Payment for order ${orderId}`,
+            orderId: orderNumber,
+            description: `Payment for order ${orderNumber}`,
         });
 
         if (zibalRes.data.result === 100) {
-            await Order.findByIdAndUpdate(orderId, {
+            await Order.findOneAndUpdate({ orderNumber }, {
                 "paymentInfo.method": "Card",
                 "paymentInfo.paymentResult.trackId": String(zibalRes.data.trackId),
                 "paymentInfo.paymentResult.status": "WaitingForVerification"
+            }, {
+                new: true
             });
 
             return res.json({ paymentUrl: `https://gateway.zibal.ir/start/${zibalRes.data.trackId}` });
