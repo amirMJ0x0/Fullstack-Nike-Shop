123935a9e6743acc906dd5c26013be8bd3065bb7
amirMJ0x0 <amirmohammad.javaheri.7d5@gmail.com>
2025-07-20 00:43:47 +0330

    Increase staleTime and add cacheTime to fetchCart query

--- frontend/src/hooks/useCartData.js
+++ frontend/src/hooks/useCartData.js
@@ -7,16 +7,17 @@ export const useCartData = () => {
     const { user } = useAuth();
 
     return useQuery({
-        queryKey: ["cart", user?._id],
-        queryFn: async () => {
-            if (user) {
-                const { data } = await api.get("/cart");
-                return data.cart;
-            } else {
-                return getCartFromLocalStorage();
-            }
-        },
-        enabled: !!user || typeof window !== "undefined", // allow localStorage read even if no user
-        staleTime: 1000 * 60, // 1 min
+      queryKey: ["cart", user?._id],
+      queryFn: async () => {
+        if (user) {
+          const { data } = await api.get("/cart");
+          return data.cart;
+        } else {
+          return getCartFromLocalStorage();
+        }
+      },
+      enabled: !!user || typeof window !== "undefined", // allow localStorage read even if no user
+      staleTime: 1000 * 60 * 3, // 3 min
+      cacheTime: 1000 * 60 * 10, // 10 min
     });
 };
